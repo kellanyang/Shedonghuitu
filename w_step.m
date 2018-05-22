2cefc34857c14767479dec9cd763fb73191776a1
kellan <kellanyang@163.com>
2018-05-22 17:29:34 +0800

    Added two other optional frequency ranges for comparison.

--- w_step.m
+++ w_step.m
@@ -3,7 +3,8 @@ CircuitConfig
 
 figure
 % w_array = (19958:5:20036)*2*pi;
-w_array = (19000:200:21000)*2*pi;
+% w_array = (19000:200:21500)*2*pi;
+w_array = (18000:200:22000)*2*pi;
 for i=1:length(w_array)
     Circuit.w = w_array(i);
     SysDefinition
