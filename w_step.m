f6cc402227087d48135825c7a814cef9c6584052
kellan <kellanyang@163.com>
2018-05-22 09:16:58 +0800

    Changed perturbation range of w.

--- w_step.m
+++ w_step.m
@@ -2,8 +2,8 @@ clear;clc;
 CircuitConfig
 
 figure
-% w_array = (19958:5:20036)*2*pi;
-w_array = (18000:200:22000)*2*pi;
+w_array = (19958:5:20036)*2*pi;
+% w_array = (18000:200:22000)*2*pi;
 for i=1:length(w_array)
     Circuit.w = w_array(i);
     SysDefinition
