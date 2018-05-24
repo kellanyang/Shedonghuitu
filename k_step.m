22b644588f9b18cba07006100d3f618825788aa4
kellan <kellanyang@163.com>
2018-05-24 10:57:48 +0800

    Changed circuit.M to a static value of 40e-6.

--- k_step.m
+++ k_step.m
@@ -4,7 +4,7 @@ CircuitConfig
 figure
 k_array = 0.1:0.02:0.3;
 for i=1:length(k_array)
-    Circuit.M = k_array(i)*sqrt(150e-6*200e-6);
+    Circuit.M = 40e-6;%k_array(i)*sqrt(150e-6*200e-6);
     SysDefinition
     step(Gss);
     hold on
