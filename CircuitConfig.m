cefde277e108ed318e080cb83fb413a08894752f
kellan <kellanyang@163.com>
2018-05-24 10:58:49 +0800

    Changed Circuit.Lp to 200e-6, Circuit.M to 40e-6, Cf to 1000e-6 and Rdc to10.

--- CircuitConfig.m
+++ CircuitConfig.m
@@ -1,20 +1,14 @@
 Circuit.w = 20000*pi*2;
 
-Circuit.Rp = 0.3118;
-Circuit.Lp = 500e-6; %200e-06;
+Circuit.Rp = 0.0;
+Circuit.Lp = 200e-6; %200e-06;
 Circuit.Cp = 1/Circuit.w^2/Circuit.Lp; %317.98e-09;%%
 
-Circuit.M= 44e-6;%44e-6;
+Circuit.M= 40e-6;%44e-6;
 
-Circuit.Rs = 0.277;
-Circuit.Ls = 152e-06;
+Circuit.Rs = 0.0;
+Circuit.Ls = 150e-06;
 Circuit.Cs = 1/Circuit.w^2/Circuit.Ls; %%
 
-Circuit.Cf = 196e-6;
+Circuit.Cf = 1000e-6;
 Circuit.Rdc = 10;
-
-
-
-Circuit.FeedbackDelay = 0.00045;
-
-
